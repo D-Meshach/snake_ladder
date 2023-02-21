8fd47acc7ba74500b901bd640bf952c0de59517c
D-Meshach <meshachdaniel456@gmail.com>
2023-02-21 09:54:55 +0530

    generate random dice

--- Program.cs
+++ Program.cs
@@ -13,8 +13,11 @@ namespace snake_ladder_upload
         {
             Console.Write("Welcome to snake and Ladder program\n");
             int counts = 0, dice_played = 0, i = 0;
+            Random random = new Random();
             while (counts <= 100)
             {
+                int dice = random.Next(1, 7);
+                counts += dice;
                 Console.Write("\n Player Position :" + counts);
                 counts++;
             }
